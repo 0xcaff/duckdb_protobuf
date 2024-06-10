0a0983b8265af38f4553d5b2a5ed445a0161e160
martin <martincharles07@gmail.com>
2024-06-10 02:23:05 -0500

    add enum handling
    and delimited list parsing
    
    next up, need to actually read the values in the protobuf files

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
+use byteorder::{BigEndian, ReadBytesExt};
 use std::error::Error;
-use std::ffi::{c_char, c_void};
+use std::ffi::{c_char, c_void, CString};
 use std::fs::File;
-use std::io::Read;
+use std::io;
+use std::io::{ErrorKind, Read};
 
-use duckdb::Connection;
-use duckdb::ffi;
 use duckdb::vtab::{
     BindInfo, DataChunk, Free, FunctionInfo, InitInfo, LogicalType, LogicalTypeId, VTab,
 };
+use duckdb::Connection;
 use duckdb_loadable_macros::duckdb_entrypoint;
 use prost::Message;
-use prost_types::{DescriptorProto, FieldDescriptorProto, FileDescriptorSet};
-use prost_types::field_descriptor_proto::Type;
+use prost_types::field_descriptor_proto::{Label, Type};
+use prost_types::{DescriptorProto, EnumDescriptorProto, FieldDescriptorProto, FileDescriptorSet};
 
 struct ProtobufVTab;
 
@@ -34,13 +35,64 @@ impl Free for ProtobufBindData {
 
 #[repr(C)]
 struct ProtobufInitData {
-    done: bool,
+    files_iterator: glob::Paths,
+    current_file: Option<LengthDelimitedRecordsReader<File>>,
+}
+
+struct LengthDelimitedRecordsReader<R: io::Read> {
+    reader: R,
+}
+
+impl<R> Iterator for LengthDelimitedRecordsReader<R>
+where
+    R: io::Read,
+{
+    type Item = Result<Vec<u8>, io::Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.try_get_next() {
+            Ok(value) => Some(Ok(value)),
+            Err(err) => {
+                if err.kind() == ErrorKind::UnexpectedEof {
+                    None
+                } else {
+                    Some(Err(err))
+                }
+            }
+        }
+    }
+}
+
+impl<R> LengthDelimitedRecordsReader<R>
+where
+    R: io::Read,
+{
+    pub fn new(reader: R) -> Self {
+        Self { reader }
+    }
+
+    pub fn try_get_next(&mut self) -> Result<Vec<u8>, io::Error> {
+        let len = self.reader.read_u32::<BigEndian>()?;
+        let mut buf = vec![0; len as usize];
+        self.reader.read_exact(&mut buf)?;
+
+        Ok(buf)
+    }
+}
+
+impl ProtobufInitData {}
+
+pub fn init(bind_params: &Parameters) -> Result<ProtobufInitData, Box<dyn Error>> {
+    Ok(ProtobufInitData {
+        files_iterator: glob::glob(bind_params.files.as_str())?,
+        current_file: None,
+    })
 }
 
 impl Free for ProtobufInitData {}
 
 struct Parameters {
-    files: glob::Paths,
+    files: String,
     message_descriptor: DescriptorProto,
     descriptors: FileDescriptorSet,
 }
@@ -62,16 +114,29 @@ pub fn get_message_matching<'a>(
     Ok(None)
 }
 
+pub fn get_enum_matching<'a>(
+    descriptor_set: &'a FileDescriptorSet,
+    name: &str,
+) -> Result<Option<&'a EnumDescriptorProto>, Box<dyn Error>> {
+    for file_descriptor in &descriptor_set.file {
+        let package_name = file_descriptor.package();
+
+        for enum_descriptor in &file_descriptor.enum_type {
+            if &format!("{}.{}", package_name, enum_descriptor.name()) == name {
+                return Ok(Some(enum_descriptor));
+            }
+        }
+    }
+
+    Ok(None)
+}
+
 impl Parameters {
     pub fn from_bind_info(bind: &BindInfo) -> Result<Self, Box<dyn Error>> {
-        let files = {
-            let files = bind
-                .get_named_parameter("files")
-                .ok_or("missing argument files")?
-                .to_string();
-
-            glob::glob(&files)?
-        };
+        let files = bind
+            .get_named_parameter("files")
+            .ok_or("missing argument files")?
+            .to_string();
 
         let descriptors = {
             let descriptor = bind
@@ -123,16 +188,58 @@ pub fn into_logical_type(
     field: &FieldDescriptorProto,
     descriptors: &FileDescriptorSet,
 ) -> Result<LogicalType, Box<dyn Error>> {
+    match field.label() {
+        Label::Optional | Label::Required => Ok(into_logical_type_single(field, descriptors)?),
+        Label::Repeated => Ok(LogicalType::list(&into_logical_type_single(
+            field,
+            descriptors,
+        )?)),
+    }
+}
+
+pub fn into_logical_type_enum(
+    enum_descriptor: &EnumDescriptorProto,
+) -> Result<LogicalType, Box<dyn Error>> {
+    let mut values = enum_descriptor.value.clone();
+    values.sort_by_key(|it| it.number());
+
+    if values[0].number() != 0 {
+        return Err("enum values must start at 0".into());
+    }
+
+    if values.len() != values.last().unwrap().number() as usize + 1 {
+        return Err("enum values must be contiguous".into());
+    }
+
+    Ok(create_enum_type(
+        &values
+            .iter()
+            .map(|it| it.name())
+            .collect::<Vec<&str>>()
+            .as_slice(),
+    ))
+}
+
+pub fn get_type_name(field: &FieldDescriptorProto) -> Result<&str, Box<dyn Error>> {
+    let type_name = field.type_name();
+    let (prefix, absolute_type_name) = type_name.split_at(1);
+    if prefix != "." {
+        return Err(format!("invalid type name: {}", type_name).into());
+    }
+
+    Ok(absolute_type_name)
+}
+
+pub fn into_logical_type_single(
+    field: &FieldDescriptorProto,
+    descriptors: &FileDescriptorSet,
+) -> Result<LogicalType, Box<dyn Error>> {
     let value = match field.r#type() {
         Type::Message => {
-            let type_name = field.type_name();
-            let (prefix, absolute_type_name) = type_name.split_at(1);
-            if prefix != "." {
-                return Err(format!("invalid type name: {}", type_name).into());
-            }
+            let type_name = get_type_name(field)?;
 
-            let message_descriptor = get_message_matching(descriptors, absolute_type_name)?
-                .ok_or(format!("message type not found: {}", absolute_type_name))?;
+            let message_descriptor = get_message_matching(descriptors, type_name)?
+                .ok_or(format!("message type not found: {}", type_name))?;
 
             LogicalType::struct_type(
                 &message_descriptor
@@ -142,6 +249,13 @@ pub fn into_logical_type(
                     .collect::<Result<Vec<(&str, LogicalType)>, Box<dyn Error>>>()?,
             )
         }
+        Type::Enum => {
+            let type_name = get_type_name(field)?;
+            let enum_descriptor = get_enum_matching(descriptors, type_name)?
+                .ok_or(format!("enum type not found: {}", type_name))?;
+
+            into_logical_type_enum(enum_descriptor)?
+        }
         Type::String => LogicalType::new(LogicalTypeId::Varchar),
         Type::Uint32 => LogicalType::new(LogicalTypeId::UInteger),
         Type::Uint64 => LogicalType::new(LogicalTypeId::UBigint),
@@ -150,7 +264,6 @@ pub fn into_logical_type(
         Type::Int32 => LogicalType::new(LogicalTypeId::Integer),
         Type::Int64 => LogicalType::new(LogicalTypeId::Bigint),
         Type::Bool => LogicalType::new(LogicalTypeId::Boolean),
-        Type::Enum => LogicalType::new(LogicalTypeId::UInteger),
         logical_type => {
             return Err(format!(
                 "unhandled field: {}, type: {}",
@@ -162,6 +275,18 @@ pub fn into_logical_type(
     };
 
     Ok(value)
+}
+
+pub fn create_enum_type(variants: &[&str]) -> LogicalType {
+    let variants = variants
+        .iter()
+        .map(|it| CString::new(it).unwrap())
+        .collect::<Vec<_>>();
+    let mut variants = variants.iter().map(|it| it.as_ptr()).collect::<Vec<_>>();
+
+    From::from(unsafe {
+        duckdb::ffi::duckdb_create_enum_type(variants.as_mut_ptr(), variants.len() as _)
+    })
 }
 
 impl VTab for ProtobufVTab {
@@ -188,19 +313,29 @@ impl VTab for ProtobufVTab {
         Ok(())
     }
 
-    fn init(_init: &InitInfo, data: *mut Self::InitData) -> duckdb::Result<(), Box<dyn Error>> {
+    fn init(init: &InitInfo, data: *mut Self::InitData) -> duckdb::Result<(), Box<dyn Error>> {
+        let bind_date = init.get_bind_data::<ProtobufBindData>();
+
         unsafe {
-            (*data).done = false;
+            let parameters = &*(&*bind_date).parameters;
+            *data = init(parameters)?;
         }
 
         Ok(())
     }
 
     fn func(func: &FunctionInfo, output: &mut DataChunk) -> duckdb::Result<(), Box<dyn Error>> {
-        let init_info = func.get_init_data::<ProtobufInitData>();
-        let bind_info = func.get_bind_data::<ProtobufBindData>();
+        let bind_info = unsafe {
+            let bind_info = func.get_bind_data::<ProtobufBindData>();
 
-        output.set_len(0);
+            &mut *bind_info
+        };
+
+        let mut init_info = unsafe {
+            let init_info = func.get_init_data::<ProtobufInitData>();
+
+            &mut *init_info
+        };
 
         // unsafe {
         //     if (*init_info).done {
