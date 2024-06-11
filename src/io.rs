a5a607a4e3b21c93027e08bbe6cc4a11a47e0888
martin <martincharles07@gmail.com>
2024-06-11 16:59:39 -0500

    split out io

---
+++ src/io.rs
@@ -0,0 +1,72 @@
+use crate::Parameters;
+use byteorder::{BigEndian, ReadBytesExt};
+use std::error::Error;
+use std::fs::File;
+use std::io;
+
+pub struct RecordsReader {
+    files_iterator: glob::Paths,
+    current_file: Option<LengthDelimitedRecordsReader<File>>,
+}
+
+impl RecordsReader {
+    pub fn new(params: &Parameters) -> Result<RecordsReader, Box<dyn Error>> {
+        Ok(RecordsReader {
+            files_iterator: glob::glob(params.files.as_str())?,
+            current_file: None,
+        })
+    }
+
+    pub fn next_message(&mut self) -> Result<Option<Vec<u8>>, Box<dyn Error>> {
+        let file_reader = if let Some(reader) = &mut self.current_file {
+            reader
+        } else {
+            let Some(next_file_path) = self.files_iterator.next() else {
+                return Ok(None);
+            };
+
+            let next_file_path = next_file_path?;
+            let next_file = File::open(&next_file_path)?;
+
+            self.current_file = Some(LengthDelimitedRecordsReader::new(next_file));
+
+            self.current_file.as_mut().unwrap()
+        };
+
+        let Some(next_message) = file_reader.try_get_next()? else {
+            self.current_file = None;
+            return Ok(None);
+        };
+
+        Ok(Some(next_message))
+    }
+}
+
+pub struct LengthDelimitedRecordsReader<R: io::Read> {
+    reader: R,
+}
+
+impl<R> LengthDelimitedRecordsReader<R>
+where
+    R: io::Read,
+{
+    pub fn new(reader: R) -> Self {
+        Self { reader }
+    }
+
+    pub fn get_next(&mut self) -> Result<Vec<u8>, io::Error> {
+        let len = self.reader.read_u32::<BigEndian>()?;
+        let mut buf = vec![0; len as usize];
+        self.reader.read_exact(&mut buf)?;
+
+        Ok(buf)
+    }
+
+    pub fn try_get_next(&mut self) -> Result<Option<Vec<u8>>, io::Error> {
+        match self.get_next() {
+            Ok(it) => Ok(Some(it)),
+            Err(err) if err.kind() == io::ErrorKind::UnexpectedEof => Ok(None),
+            Err(err) => Err(err.into()),
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod descriptors;
+mod io;
 
 use core::fmt;
 use std::collections::{HashMap, HashSet};
@@ -6,12 +7,12 @@ use std::error::Error;
 use std::ffi::{c_char, c_void};
 use std::fmt::Formatter;
 use std::fs::File;
-use std::io;
 use std::io::Read;
 use std::ops::{Deref, DerefMut};
 
 use crate::descriptors::{FieldDescriptorProtoExt, FileDescriptorSetExt};
-use byteorder::{BigEndian, ReadBytesExt};
+use crate::io::RecordsReader;
+use byteorder::ReadBytesExt;
 use duckdb::ffi;
 use duckdb::ffi::{
     duckdb_list_entry, duckdb_list_vector_get_child, duckdb_list_vector_reserve,
@@ -68,73 +69,6 @@ impl<T> Free for Handle<T> {
             }
 
             drop(Box::from_raw(self.inner));
-        }
-    }
-}
-
-struct RecordsReader {
-    files_iterator: glob::Paths,
-    current_file: Option<LengthDelimitedRecordsReader<File>>,
-}
-
-impl RecordsReader {
-    pub fn new(params: &Parameters) -> Result<RecordsReader, Box<dyn Error>> {
-        Ok(RecordsReader {
-            files_iterator: glob::glob(params.files.as_str())?,
-            current_file: None,
-        })
-    }
-
-    fn next_message(&mut self) -> Result<Option<Vec<u8>>, Box<dyn Error>> {
-        let file_reader = if let Some(reader) = &mut self.current_file {
-            reader
-        } else {
-            let Some(next_file_path) = self.files_iterator.next() else {
-                return Ok(None);
-            };
-
-            let next_file_path = next_file_path?;
-            let next_file = File::open(&next_file_path)?;
-
-            self.current_file = Some(LengthDelimitedRecordsReader::new(next_file));
-
-            self.current_file.as_mut().unwrap()
-        };
-
-        let Some(next_message) = file_reader.try_get_next()? else {
-            self.current_file = None;
-            return Ok(None);
-        };
-
-        Ok(Some(next_message))
-    }
-}
-
-struct LengthDelimitedRecordsReader<R: io::Read> {
-    reader: R,
-}
-
-impl<R> LengthDelimitedRecordsReader<R>
-where
-    R: io::Read,
-{
-    pub fn new(reader: R) -> Self {
-        Self { reader }
-    }
-
-    pub fn get_next(&mut self) -> Result<Vec<u8>, io::Error> {
-        let len = self.reader.read_u32::<BigEndian>()?;
-        let mut buf = vec![0; len as usize];
-        self.reader.read_exact(&mut buf)?;
-
-        Ok(buf)
-    }
-
-    pub fn try_get_next(&mut self) -> Result<Option<Vec<u8>>, io::Error> {
-        match self.get_next() {
-            Ok(it) => Ok(Some(it)),
-            Err(err) if err.kind() == io::ErrorKind::UnexpectedEof => Ok(None),
-            Err(err) => Err(err.into()),
         }
     }
 }
